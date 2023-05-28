067218437190aac1919d3fc2c490a4e7af0655a9
Rajmandal17 <Rajmandal147@gmail.com>
2023-05-28 18:02:56 +0530

    pet-app

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Bootstrap Footer Example</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+<body>
+
+<footer class="footer">
+  <div class="container text-center">
+    <span class="text-muted">© 2023 Your Company. All rights reserved.</span>
+  </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+</body>
+</html>
